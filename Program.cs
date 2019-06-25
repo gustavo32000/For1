234cd35cb9bbc7d897c5690b6bd124d6001a8ba8
gugu <gustavocorrea2032@gmail.com>
2019-06-25 12:37:25 -0300

    For1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace For1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int qtd = 0;    
+
+
+        for (int n = 1 ; n <=50 ; n++){
+             if (n % 2 == 0){
+                 Console.WriteLine("O numero " + n +" é par!");
+                    qtd++;
+
+             }
+        }
+             Console.BackgroundColor = ConsoleColor.Black;
+             Console.ForegroundColor = ConsoleColor.White;
+             Console.WriteLine ("Entre 0 e 50 há " +qtd+ " pares");   
+
+        }
+        
+         
+        }
+    }
+
